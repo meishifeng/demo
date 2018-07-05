f06f53efdcd145a4929b353062e0e2507b351b7f
meishifeng <546061343@qq.com>
2018-07-05 22:20:16 +0800

    '增加注释'

---
+++ project-demo/web/src/test/java/com/meishifeng/demo/web/LoginControllerTest.java
@@ -0,0 +1,12 @@
+package com.meishifeng.demo.web;
+
+/**
+ * <ul>
+ * <li>描述类的作用</li>
+ * <li>1.方法一说明</li>
+ * <li>2.方法二说明</li>
+ * <li>User:meishifeng Date:2018/7/5 Time:21:01</li>
+ * </ul>
+ */
+public class LoginControllerTest {
+}
